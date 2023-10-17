f34779394be1c1ee4a8af880fd52e9cb09119e67
Santiago Martínez <xsantysxx@gmail.com>
2023-10-17 06:19:34 -0500

    Feature: First problem solution

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-challenge-2
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clojure_challenge/core.clj
@@ -0,0 +1,30 @@
+(ns clojure-challenge.core)
+
+(def invoice (clojure.edn/read-string (slurp "src/invoice.edn")))
+
+"This function receives an invoice and return the items what satisfaces some of the two conditions"
+(defn filter-items-by-iva-or-retention [invoice]
+  (->> invoice
+       :invoice/items
+       (filter (fn [item]
+                 (let [taxes (-> item :taxable/taxes) retentions (-> item :retentionable/retentions)]
+                   (if (and (some #(= 19 (:tax/rate %)) taxes)
+                            (some #(= 1 (:retention/rate %)) retentions)
+                            )
+                     false
+                     true
+                     )
+                   )
+                 ))))
+
+
+
+
+
+(def filtered-items (filter-items-by-iva-or-retention invoice))
+
+(defn -main
+  [& args]
+  (println filtered-items)
+  )
+
